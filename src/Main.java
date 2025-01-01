ea11a03369425f575a2090dc0b138a04e2e8c6d3
Vedika894 <vjain894@yahoo.com>
2025-01-01 16:22:21 -0500

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
+   ArrayList <Double> expenses = new ArrayList<>();
+   expenses.add(74.46);
+   expenses.add(63.99);
+   expenses.add(10.57);
+   expenses.add(81.73);
 
+   double total =0;
+
+   //illiterate over expenses
+        for (int i =0; i < expenses.size(); i++){
+            total += expenses.get(i);
+        }
+        System.out.println(total);
     }
 }
