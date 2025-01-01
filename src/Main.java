474e74449bc5bac05c7a45a0dc28a6bffc5bfd44
Vedika894 <vjain894@yahoo.com>
2024-12-31 21:18:33 -0500

    commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+java.util.Arrays;
+
+public class Main {
+   public static void main (String[] args){
+       String[] students = {"Sade" , "Alexus", "Sam ", "Koma"};
+       int[] mathScores = new int[4];
+
+
+       mathScores[0] = 64;
+       mathScores[2] = 76;
+       mathScores[1] = 57;
+       mathScores[3] = 98;
+
+       //for loop to illiterate over
+    }
+}
