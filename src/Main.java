b5419c5161f015202046690c05710575c0934332
Vedika894 <vjain894@yahoo.com>
2025-01-02 23:16:57 -0500

    commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,19 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-       
+
+//        Bank bankOfGods = new Bank();
+//        System.out.println(bankOfGods.accountOne.getBalance());
+//        bankOfGods.accountOne.setBalance(5000);
+//        System.out.println(bankOfGods.accountOne.getBalance());
+//        System.out.println(bankOfGods.accountOne.getMonthlyInterest());
+//
+//        //close account
+//        bankOfGods.accountOne.closeAccount();
+//
+//        //apply fees
+//        bankOfGods.accountTwo.applyFees(50);
+//        System.out.println(bankOfGods.accountTwo.getBalance());
+
     }
 }
