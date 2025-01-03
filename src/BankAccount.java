7850c2e8c18cbf6a65217628e4346380ae083c06
Vedika894 <vjain894@yahoo.com>
2025-01-02 21:51:33 -0500

    commit

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -4,9 +4,9 @@ public class BankAccount {
     private String accountHolder;
     private double balance;
 
-    public BankAccount(String accountHolder,int balance ){
+    public BankAccount(String accountHolder){
         this.accountHolder = accountHolder;
-        this.balance = balance;
+        this.balance = 0.0;
     }
     //setters for both fields
     public void setAccountHolder(String holder){
@@ -28,27 +28,42 @@ public class BankAccount {
    }
 
    public void withdraw (double withdraw){
-        if (balance >= 0){
-            balance -= withdraw;
-       }
-        if (withdraw > balance){
-            System.out.println("error");
+        if ( withdraw <= this.balance && withdraw >= 0){
+            this.balance -= withdraw;
+        }
+        else{
+            System.out.println("Insufficient funds");
         }
         }
        public void printSummary (){
            System.out.printf("Account Holder: %s \n",accountHolder);
            System.out.printf("Balance : %.2f \n", balance);
        }
+      //could not create the transfer method by myself,
+    public void transfer(BankAccount otherAccount, double amount){
+        //withdraw
+        if (amount > this.balance){
+            System.out.println("not enough funds");
+            return;
+        }
+        //withdraw from the account
+        this.withdraw(amount);
 
+        //deposit in the other account
+        otherAccount.deposit(amount);
+    }
     public static void main(String[] args) {
-      BankAccount account1 = new BankAccount("Vedika", 100000);
-      BankAccount account2 = new BankAccount("Bhavna", 100000);
+      BankAccount account1 = new BankAccount("Vedika");
+      BankAccount account2 = new BankAccount("Bhavna");
      //calling each method
-        account1.deposit(40000);
-        account2.deposit(40000);
+        account1.deposit(100.0);
+        account2.deposit(50.0);
 
         account1.withdraw(0);
         account2.withdraw(0);
+
+        account1.transfer(account1, 30.0);
+
 
         account1.printSummary();
         account2.printSummary();
