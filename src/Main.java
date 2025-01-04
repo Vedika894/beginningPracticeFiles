08f1b0a73a01d563066e02fd661c2a3c9a399b02
Vedika894 <vjain894@yahoo.com>
2025-01-03 19:19:08 -0500

    commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+      //inheritance = the process when one class acquires the attributes and methods of another
+      Car car = new Car();
+      car.go();
+
+      Bicycle bike = new Bicycle();
+
+      bike.stop();
+
+        System.out.println(car.speed);
+        System.out.println(bike.speed);
+
+        System.out.println(car.doors);
+        System.out.println(bike.pedals);
+
+    }
+}
