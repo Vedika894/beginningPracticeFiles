91f8306f346751a0e68541a6fbd9b6175b5965c3
Vedika894 <vjain894@yahoo.com>
2025-01-04 18:47:46 -0500

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,37 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
  public static void main (String[] args){
-     String[] array = {"one", "two", "three", "four", "five"};
-     String[]
+     //we are looping through the array from the last index to the first index (0)
+     //On each iteration, we print the element at the current index i in reverse order
+     String[] words = {"one", "two", "three", "four", "five"};
+
+     //loop trough the array in reverse order and print the elements
+     for ( int i = words.length -1; i >=0 ; i--){
+         System.out.println(words[i]);
+
+
+
+         //didn;t learn stacks yet
+
+         //do the same thing, but using another
+         //method
+         String[] words2 = {"one", "two", "three", "four"};
+
+         //creating a stack to hold the words
+         Stack<String> stack = new Stack<>();
+
+         //push all the words into the stack
+         for ( String j : words2){
+             stack.push(j);
+         }
+       //pop and print each form the stack ( in reverse order)
+         while (!stack.isEmpty()){
+             System.out.println(stack.pop());
+         }
+     }
    }
 }
